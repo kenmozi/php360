15d47b57b9bd12515c5867653f5b4603724ab99b
Moise W. CONVOLBO <wend_kuuni@yahoo.fr>
2015-09-11 14:40:47 +0800

    Create Ch1_5.php

---
+++ Ch1_5.php
@@ -0,0 +1,17 @@
+<?php
+/* les variables nous permettent de sauvegarder les donnees:
+* On les appelle des variable car leurs valeurs peuvent changer
+* a tout moment: Par example si votre presnom est Salif, quelqu'un d'autre 
+* a Edith comme prenom
+*/
+
+  $nomDeFamille="OUEDRAOGO";
+  $prenom="Salam";
+  $age=34;
+
+
+  $nomDeFamille="KAM";
+  $prenom="Moussa";
+  $age=13;
+  $classe: "CP2";
+?>
