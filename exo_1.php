86a30dda2ab9363caab054403c155dda8bd91ca0
Moise W. CONVOLBO <wend_kuuni@yahoo.fr>
2016-02-15 17:19:03 +0800

    Create exo_1.php

---
+++ exo_1.php
@@ -0,0 +1,20 @@
+<?php
+/*
+lequelles des variables suivantes ne repondent pas au criteres de la norme PHP sur les noms de variable?
+
+*/
+
+$True
+$ii
+$_ii
+$$ma_var
+$12
+$w8&
+$@
+variable1
+$var2
+$TrueFalse
+$dodo
+$While
+
+?>
